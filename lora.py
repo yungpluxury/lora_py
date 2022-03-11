960748463df40f655ff615843fcf012c5298ad05
yungpluxury <79522333+yungpluxury@users.noreply.github.com>
2022-03-11 14:41:05 +0300

    Update lora.py

--- lora.py
+++ lora.py
@@ -1,12 +1,12 @@
 import serial;
 import requests;
 import json;
-serialPort=serial.Serial(port='COM4',baudrate=115200);
+serialPort=serial.Serial(port='/dev/ttyUSB0',baudrate=115200);
 while True:
     print("");
     data=serialPort.readline().decode('CP866');
     jsonPart=json.dumps({"data": data});
-    req = requests.post('http://172.16.117.193:80/gettingData', headers={'Content-Type': 'application/json'}, data=jsonPart);
+    req = requests.post('http://172.16.117.193:3000/gettingData', headers={'Content-Type': 'application/json'}, data=jsonPart);
     print(data);
     print (req.text);
 serialPort.close();
